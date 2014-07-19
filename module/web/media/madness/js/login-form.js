1c32ad1e6b26606fc49c27581b0f29f62062ad55
ASCIIteapot <asciiteapot@gmail.com>
2014-07-19 23:16:39 +0600

    disable react for login page and now it works

---
+++ module/web/media/madness/js/login-form.js
@@ -0,0 +1,33 @@
+/**
+ * Created by Developer on 19.07.14.
+ * @return {boolean}
+ */
+
+$(document).ready( function() {
+    $('#form-signin').submit( function(form) {
+//      reuest login from pyload api
+        var formData=$(form.target).serialize();
+        $.ajax({
+            type: "POST",
+            url: '/api/login',
+            dataType: 'json',
+            data: formData
+        })
+        .fail(function(jqXHR, textStatus, errorThrown){
+
+        })
+        .done(function( data, textStatus, jqXHR ) {
+            if(data == false){
+                // login data incorrect show error message
+                $('#invalid-user-alert').removeClass('hidden');
+            }
+            else{
+                // login data is correct
+                // process for submit
+                console.message('login success');
+                form.target.submit();
+            }
+        });
+        return false;
+    });
+});
